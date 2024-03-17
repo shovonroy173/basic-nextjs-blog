7d4f3683d8ae58e514a9a7d774d0aa51c69047ad
shovonroy173 <shovonroy2003@gmail.com>
2024-03-17 23:50:37 +0530

    5th commit

---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const notFound = () => {
+  return (
+    <div>
+      notFound
+      <Link href="/">Return to home!!</Link>
+    </div>
+  )
+}
+
+export default notFound
