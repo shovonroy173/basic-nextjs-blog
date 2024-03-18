0a27ee49c5b50c492b1d20faa7fe6523ed160daa
shovonroy173 <shovonroy2003@gmail.com>
2024-03-18 19:44:18 +0530

    7th commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='px-14 flex justify-between items-center h-10 w-full font-bold text-cyan-950 bg-gray-300'>
+        <p>Logo <span>.</span></p>
+        <p>Alrights reserved to @cs202</p>
+    </div>
+  )
+}
+
+export default Footer
