a452fd6267e320a1f13d2d7329efbeefe99eef73
shovonroy173 <shovonroy2003@gmail.com>
2024-03-18 19:45:32 +0530

    9th commit

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,9 +8,10 @@ export const connectToDb = async () => {
       console.log("Using existing connection");
       return;
     }
-    const db = await mongoose.connect(process.env.MONGO_URI || `mongodb+srv://shovon:nwXHh1yt5Gj4fj7P@store-cluster.o7kflav.mongodb.net/nextjs-blog-media?retryWrites=true&w=majority`);
+    const db = await mongoose.connect(process.env.MONGO_URI || `mongodb+srv://shovon:nwXHh1yt5Gj4fj7P@store-cluster.o7kflav.mongodb.net/nextjs-blog-media?retryWrites=true&w=majority` );
+    console.log("COnnected to DB!!");
     connection.isConnected = db.connections[0].readyState;
-  } catch (error) {
+  } catch (error) { console.log("Not connected to DB!");
     console.log(error);
     throw new Error(error);
   }
