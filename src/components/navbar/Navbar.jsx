3369e7d2e0b23c32c4c46ad848468b548ddd5569
shovonroy173 <shovonroy2003@gmail.com>
2024-03-29 21:32:40 +0530

    22th commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = async() => {
   const session = await auth();
   // console.log(session);
   return (
-    <div className='px-14 flex justify-between items-center h-20'>
+    <div className='px-14 flex justify-between items-center h-20  bg-gray-300'>
       <Link href="/">Logo <span>.</span></Link>
       <div>
       <Links session={session}/>
