3421f759398570361f87fbff6f10195ed6dd7087
shovonroy173 <shovonroy2003@gmail.com>
2024-03-30 23:44:07 +0530

    24 th commit

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -1,19 +1,25 @@
 "use client"
-import React from "react";
+import React, { useEffect } from "react";
 import { useFormState } from 'react-dom';
 import { login } from '@/lib/actions';
+import { useRouter } from "next/navigation";
 
-const LoginForm = () => {
+const LoginForm = ({session}) => {
     const [state , formLogin] = useFormState(login , {});
-    console.log(state);
+    console.log("LINE AT 9" , session);
+    const router = useRouter();
+    useEffect(()=>{
+      session && router.push("/")
+    } , [session])
 
   return (
     <div>
-      <form action={formLogin}>
+      <form action={formLogin} className="flex flex-col space-y-10 w-[500px]">
         <input type="text" placeholder="username" name="name" />
         <input type="password" placeholder="password" name="password" />
         <button>Login with credentials</button>
       </form>
+      {state.error}
     </div>
   );
 };
